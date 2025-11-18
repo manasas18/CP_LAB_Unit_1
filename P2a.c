139397a5e02ed80865b809d53adb7a74fa1ac5eb
manasas18 <kalashivakalashiva1@gmail.com>
2025-11-18 20:47:17 +0530

    Create P2a.c

---
+++ P2a.c
@@ -0,0 +1,15 @@
+//c program for display different data types 
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 3.14;
+    char c = 'A';
+    double d = 25.9876;
+
+    printf("Integer value: %d\n", a);
+    printf("Float value: %.2f\n", b);
+    printf("Character value: %c\n", c);
+    printf("Double value: %.4lf\n", d);
+
+    return 0;
