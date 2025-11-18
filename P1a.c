9d789d28ccd81cc8cb179c8419bbe6a3273dff97
manasas18 <kalashivakalashiva1@gmail.com>
2025-11-18 20:40:05 +0530

    Create P1a.c

---
+++ P1a.c
@@ -0,0 +1,13 @@
+\\c program for receving an input from the user of runtime and printing it
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("You entered: %d", num);
+
+    return 0;
+}
