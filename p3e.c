790476b7fc0816b4af182e7e8897f4632e1a0f28
manasas18 <kalashivakalashiva1@gmail.com>
2025-11-18 21:00:26 +0530

    Create p3e.c

---
+++ p3e.c
@@ -0,0 +1,12 @@
+//logical opertor
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10;
+
+    printf("a && b : %d\n", (a && b));
+    printf("a || b : %d\n", (a || b));
+    printf("!a     : %d\n", (!a));
+
+    return 0;
+}
