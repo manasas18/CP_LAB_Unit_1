e760688cae37eae92307344b78821fba5d944f2a
manasas18 <kalashivakalashiva1@gmail.com>
2025-11-18 21:23:32 +0530

    Create p7.c

---
+++ p7.c
@@ -0,0 +1,18 @@
+// cprogram to check even or odd
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input a number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check even or odd
+    if (num % 2 == 0)
+        printf("%d is even.\n", num);
+    else
+        printf("%d is odd.\n", num);
+
+    return 0;
+}
